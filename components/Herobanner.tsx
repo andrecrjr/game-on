639351602e1558a88a2b15a45d69992c28a7a0db
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-02-29 22:48:30 -0300

    - improve herobanner

--- components/Herobanner.tsx
+++ components/Herobanner.tsx
@@ -5,29 +5,21 @@ import Link from 'next/link'
 import { cva } from 'class-variance-authority'
 
 const HeroBanner = () => {
-
   return (
-    <section className=" bg-gray-800 text-white md:px-8">
-      <div className="w-screen md:container md:mx-auto flex 
-              flex-col md:flex-row items-center md:justify-between">
-        <div className="md:min-w-1/2 py-8 lg:py-0">
-          <h1 className="text-4xl md:text-left font-bold pt-8 md:pt-4">Steam ON</h1>
-          <p className="text-md mt-8 md:mt-4">
-            A Steam ON é um client minimalista que permite que você acesse dados e
-            funcionalidades do Steam. Você pode usar a Steam ON para
-            obter notícias, estatísticas, conquistas, perfis, amigos e jogos de
-            qualquer usuário ou jogo do Steam.
+    <section className=" bg-gray-800 text-white md:px-8 mt-12 md:mt-0">
+      <div className="items-center md:flex md:flex-row  md:justify-between">
+        <div className="content md:w-1/2 py-8 px-4 lg:py-0 lg:px-4">
+          <h1 className="title text-4xl md:text-left font-bold pt-8 md:pt-4">Steam ON</h1>
+          <p className="description text-md mt-8 md:mt-4">
+            Steam ON is a minimalist client that lets you access Steam data and functionalities. Use it to explore news, stats, achievements, profiles, friends, and games for any Steam user or game.
           </p>
-          <Button className='bg-green-600 text-white px-6 py-3 mt-6 rounded'>
-          <Link href={"/login"}
-          >
-            Sign In
-          </Link>
+          <Button className="btn bg-green-600 text-white px-6 py-3 mt-6 rounded">
+            <Link href="/login">Sign In</Link>
           </Button>
         </div>
-        <section className="w-1/2 relative">
+        <section className="image-container md:w-1/2 hidden md:inline-block relative ">
           <Image
-            src={'/public/steam-on-logo.png'}
+            src="/public/steam-on-logo.png"
             alt="Steam logo"
             width={400}
             height={400}
@@ -36,7 +28,7 @@ const HeroBanner = () => {
         </section>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default HeroBanner
