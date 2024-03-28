4d3786865c893563b6f05422efc7b9dfdb449b73
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-03-27 23:25:22 -0300

    fix: add metadata to profile user

---
+++ app/(authenticated-pages)/layout.tsx
@@ -0,0 +1,15 @@
+import { getServerSession } from "next-auth";
+import { getAuthOptions } from "../api/auth/[...nextauth]/route";
+import RootLayout from "../layout";
+
+
+
+export async function generateMetadata() {
+    const session = await getServerSession(getAuthOptions(undefined))
+     return {
+        title:`${session?.user.name}'s Page`,
+        description: `Game's Library from ${session?.user.name}`
+     }
+}
+
+export default RootLayout
