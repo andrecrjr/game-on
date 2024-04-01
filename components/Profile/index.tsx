ff7b396205ea177472f5cd9d2552ac96c2bb4fdc
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-04-01 20:57:42 -0300

    - refactoring tables

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -3,10 +3,9 @@ import Column from "../Grid/Column";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { getServerSession } from "next-auth";
 import { getAuthOptions } from "@/app/api/auth/[...nextauth]/route";
-import { convertTiming } from "@/app/utils";
-import Image from "next/image";
 import Title from "../Title";
-import RecentlyPlayedTable from "./RecentlyPlayedTable";
+import RecentlyPlayedTable from "../Tables/RecentlyPlayedTable";
+import FavoriteTable from "../Tables/FavoriteTable";
 
 const ColumnProfile = async () => {
     const session = await getServerSession(getAuthOptions(undefined))
@@ -21,13 +20,7 @@ const ColumnProfile = async () => {
                             <AvatarFallback>{session?.user?.username}</AvatarFallback>
                     </Avatar>
                     <RecentlyPlayedTable />
-                    <section className="self-center md:self-auto">
-                        <h2 className='font-bold text-gray-200'>Favorite Game</h2>
-                        <Image fetchPriority="high" className='mt-2' src={session?.user?.gamesLibraryData && session?.user?.gamesLibraryData.mostPlayedData.avatarHeader || ""}
-                         loading="eager" alt={session?.user?.gamesLibraryData && session?.user?.gamesLibraryData.mostPlayedData.name || ''} width={200} height={60}/>
-                        <p className="text-sm mt-2"><strong>{session?.user?.gamesLibraryData && session?.user?.gamesLibraryData.mostPlayedData.name}</strong></p>
-                        <p className="text-sm text-gray-600" title="Time played in this game">⏲️ {session?.user?.gamesLibraryData && convertTiming(session?.user?.gamesLibraryData.mostPlayedTime.playtime_forever)}</p>
-                    </section>
+                   <FavoriteTable />
                 </section>
             </Column>);
 };
---
+++ components/Tables/FavoriteTable.tsx
@@ -0,0 +1,21 @@
+import { getAuthOptions } from "@/app/api/auth/[...nextauth]/route";
+import { convertTiming } from "@/app/utils";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+import React from "react";
+
+
+const FavoriteTable = async () => {
+
+    const session = await getServerSession(getAuthOptions(undefined))
+
+    return  (<section className="self-center md:self-auto">
+                <h2 className='font-bold text-gray-200'>Favorite Game</h2>
+                <Image fetchPriority="high" className='mt-2' src={session?.user?.gamesLibraryData && session?.user?.gamesLibraryData.mostPlayedData.avatarHeader || ""}
+                    loading="eager" alt={session?.user?.gamesLibraryData && session?.user?.gamesLibraryData.mostPlayedData.name || ''} width={200} height={60}/>
+                <p className="text-sm mt-2"><strong>{session?.user?.gamesLibraryData && session?.user?.gamesLibraryData.mostPlayedData.name}</strong></p>
+                <p className="text-sm text-gray-600" title="Time played in this game">⏲️ {session?.user?.gamesLibraryData && convertTiming(session?.user?.gamesLibraryData.mostPlayedTime.playtime_forever)}</p>
+            </section>);
+};
+
+export default FavoriteTable;
