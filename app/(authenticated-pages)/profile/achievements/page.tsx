e421674d148ec526658aa2cf96ef752172da539d
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-04-02 23:18:18 -0300

    feature: add achievement page by game

--- app/(authenticated-pages)/profile/achievements/page.tsx
+++ app/(authenticated-pages)/profile/achievements/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { getAuthOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getUserAchievement } from "@/app/services";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 
 type Props = {};
 
@@ -13,12 +14,19 @@ export default async function AchivementPage({}: Props) {
   if(data.length > 0)
   return( <section>
     <h4>Achievements</h4>
-    <section>
+    <section className="overflow-hidden pr-6">
       {data.map(game=>{
-        return (<section key={game.gameName}>
+        return (
+        <section key={game.gameName}>  
           <p>{game.gameName}</p>
-          
-          
+        <section className="overflow-x-scroll mb-8">
+          <ul className="list-none flex w-fit">{game.achievements.map(achievement=>
+            <li key={achievement.displayName} className="w-20 mr-6">
+              <Image src={achievement.achieved ? achievement.icon : achievement.icongray} width="80" height={80} alt={achievement.name}/>
+            </li>)
+          }
+          </ul>
+        </section>
         </section>)
       })}
     </section>
