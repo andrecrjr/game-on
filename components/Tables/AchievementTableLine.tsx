deea50e80195e1f8f11fe3fd75d6c6ff859371a7
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-04-10 22:47:07 -0300

    arranging tableline

--- components/Tables/AchievementTableLine.tsx
+++ components/Tables/AchievementTableLine.tsx
@@ -8,15 +8,19 @@ type Props = {
 };
 
 export const AchievementTableLine = ({game, indexGame}: Props) => {
-  return (<>
-        {game.achievements.map(achievement=>
-            <li key={achievement.displayName} className="w-20 mr-6">
-                <div className="relative group">
-                    <Image src={achievement.achieved ? achievement.icon : achievement.icongray} 
-                    className={`${!achievement.achieved && "grayscale" || "" }`} width="80" height={80} alt={achievement.name}/>
-                     <p className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">{achievement.description}</p>
-                </div>
-            </li>)
+  return (
+    <>
+        {game.achievements.map(
+            achievement=>
+                <li key={achievement.displayName} className="w-20 mr-6">
+                    <div className="relative group">
+                        <Image src={achievement.achieved ? achievement.icon : achievement.icongray} 
+                        className={`${!achievement.achieved && "grayscale" || "" }`} width="80" height={80} alt={achievement.name}/>
+                        <p className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">{achievement.description}</p>
+                    </div>
+                </li>
+            )
         }
-  </>)
+    </>
+  )
 };
