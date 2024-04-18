8ba6667b297eeccfcdabb641d508ace400c440b9
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-04-17 23:15:23 -0300

    - update constrast achievements

--- components/Tables/AchievementsTable/AchievementTableLine.tsx
+++ components/Tables/AchievementsTable/AchievementTableLine.tsx
@@ -23,10 +23,10 @@ export const AchievementTableLine = ({game}: Props) => {
                         <Tooltip>
                           <TooltipTrigger asChild>
                             <Image src={achievement.achieved ? achievement.icon : achievement.icongray} 
-                            className={`${!achievement.achieved && "grayscale" || "" }`} width="80" height={80} alt={achievement.name}/>
+                            className={`${!achievement.achieved && "grayscale contrast-50" || "contrast-100" }`} width="80" height={80} alt={achievement.name}/>
                         </TooltipTrigger>
                          <TooltipContent>
-                           <p className="font-bold"> {achievement.description}</p>
+                           <p className="font-bold p-3">{achievement.achieved ? "Unlocked:" : "Locked:"}{"\n"} {achievement.description}</p>
                         </TooltipContent>
                         </Tooltip>
                     </TooltipProvider>
