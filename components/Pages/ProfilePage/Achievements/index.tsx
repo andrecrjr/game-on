a50a5569c563c11ece8dc1e6fd5dba382f7c9165
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-04-24 23:28:50 -0300

    - adicionando pagination by shadcn ui

--- components/Pages/ProfilePage/Achievements/index.tsx
+++ components/Pages/ProfilePage/Achievements/index.tsx
@@ -4,6 +4,7 @@ import AchievementsTable from "@/components/Tables/AchievementsTable"
 import { Button } from "@/components/ui/button"
 import { getServerSession } from "next-auth"
 import Link from "next/link"
+import { PaginationAchivements } from './PaginationAchivements';
 
 type Props = {
   params: {id:string}
@@ -20,8 +21,7 @@ export default async function AchievementPage({params}: Props) {
       <section className="flex flex-col w-screen md:w-auto md:pr-6">
         <h4 className="mb-4 pl-4">Achievements - Page {currentPage}  of {totalPages}</h4>
           <section className="flex justify-between px-4 pb-4">
-            {<Button><Link href={`/profile/achievements/${parseInt(params.id)-1}`}>Anterior</Link></Button>}
-            <Button className="self-end"><Link href={`/profile/achievements/${parseInt(params.id)+1}`}>Próximo</Link></Button>
+            <PaginationAchivements id={params.id} totalPages={totalPages}/>
           </section>
           <section className="px-4">
             <AchievementsTable achievements={achievements} />
