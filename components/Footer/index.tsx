a8bf087bb72e36fb10dff1bf8035b374e2822b60
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-05-20 21:17:54 -0300

    - fix: spacing

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ export function Footer(){
     return( 
         <footer className="mx-auto mt-auto min-h-8 py-3">
           <p className="text-center">by <Link href="https://andrecrjr.github.io/">Acjunior</Link></p>
-          <p>User data from 
+          <p>User data from{" "} 
             <Link href={"https://developer.valvesoftware.com/wiki/Steam_Web_API"}>Steam Web API</Link> and Game Data from{" "}
             <Link href="https://steamspy.com/">Steam Spy</Link>
           </p>
