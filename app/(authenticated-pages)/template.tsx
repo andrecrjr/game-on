c90e7bbc11536793f773b6f27a1a8a85821b5bce
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-07-14 19:32:25 -0300

    refactor(layout): simplify grid layout by removing aside column
    Remove AsideColumnProfile component and adjust grid layout to use full width for main content. This improves consistency and reduces unnecessary complexity in the layout structure.

--- app/(authenticated-pages)/template.tsx
+++ app/(authenticated-pages)/template.tsx
@@ -16,9 +16,8 @@ export default async function Template({ children }: TemplateProps) {
   return (
     <>
       <main className="pt-10 mx-auto w-full">
-        <Grid className="flex-col-reverse md:divide-x-2 md:divide-gray-200 relative">
-          <Column className="md:w-9/12">{children}</Column>
-          <AsideColumnProfile />
+        <Grid className="flex-col-reverse md:divide-gray-200 relative">
+          <Column className="w-full">{children}</Column>
         </Grid>
       </main>
     </>
