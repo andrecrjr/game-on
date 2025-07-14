c2a24e13f7e97b5b1d9c0f6b91efc426c05978c6
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-07-14 18:36:21 -0300

    style: fix indentation and enable biome vcs integration
    Enable biome's version control integration and standardize indentation in layout.tsx to improve code consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,29 +9,29 @@ import { Footer } from '@/components/Footer';
 const inter = Space_Grotesk({ subsets: ['latin'] });
 
 export async function generateMetadata() {
-	const session = await getServerSession(getAuthOptions(undefined));
-	if (session)
-		return {
-			title: `${session?.user.name}'s Page`,
-			description: `Game's Library from ${session?.user.name}`,
-		};
-	return {
-		title: 'Gamer On - Minimalist Steam Watcher',
-		description: 'Steam User Data Watcher',
-	};
+  const session = await getServerSession(getAuthOptions(undefined));
+  if (session)
+    return {
+      title: `${session?.user.name}'s Page`,
+      description: `Game's Library from ${session?.user.name}`,
+    };
+  return {
+    title: 'Gamer On - Minimalist Steam Watcher',
+    description: 'Steam User Data Watcher',
+  };
 }
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className + ' min-h-screen flex flex-col'}>
-				<SessionContext>{children}</SessionContext>
-				<Footer />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={`${inter.className} min-h-screen flex flex-col`}>
+        <SessionContext>{children}</SessionContext>
+        <Footer />
+      </body>
+    </html>
+  );
 }
